e493f89149dba07ced5c463df4eacd3117af6da4
Espen Braastad <espen@varnish-software.com>
2021-08-04 20:17:00 +0200

    Set content-type on errors

--- http.go
+++ http.go
@@ -77,7 +77,7 @@ func (h *HTTP) Init() (err error) {
 
 func CacheControl(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "must-revalidate, public, max-age=86400")
+		w.Header().Set("Cache-Control", "max-age=86400")
 		h.ServeHTTP(w, r)
 	})
 }
@@ -191,6 +191,7 @@ func (h *HTTP) Error(w http.ResponseWriter, r *http.Request, internal string, ex
 	// Disregard any request body there is
 	io.Copy(ioutil.Discard, r.Body)
 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(statusCode)
 	io.WriteString(w, external)
 	return
