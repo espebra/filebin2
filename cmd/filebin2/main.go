9b7b1fce38b15e92a78e0da975809b5e24484fa3
Espen Braastad <espen@varnish-software.com>
2026-01-25 18:52:29 +0100

    Add --version flag and FILEBIN_ environment variable support
    Add --version flag that prints the application name, version (from git
    tag), and commit hash.
    
    Add support for configuring all options via environment variables with
    the FILEBIN_ prefix. Command line flags take precedence over environment
    variables.

--- cmd/filebin2/main.go
+++ cmd/filebin2/main.go
@@ -23,6 +23,12 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// Version information (set via ldflags at build time)
+var (
+	version = "dev"
+	commit  = "unknown"
+)
+
 var (
 	// Various
 	contactFlag             = flag.String("contact", "", "The contact information, such as an email address, that will be shown on the website for people that want to get in touch with the service owner.")
@@ -57,18 +63,18 @@ var (
 	idleTimeoutFlag       = flag.Duration("idle-timeout", 30*time.Second, "Idle timeout for the HTTP server")
 
 	// Database
-	dbHostFlag         = flag.String("db-host", os.Getenv("DATABASE_HOST"), "Database host")
-	dbPortFlag         = flag.String("db-port", os.Getenv("DATABASE_PORT"), "Database port")
-	dbNameFlag         = flag.String("db-name", os.Getenv("DATABASE_NAME"), "Name of the database")
+	dbHostFlag         = flag.String("db-host", "", "Database host")
+	dbPortFlag         = flag.String("db-port", "", "Database port")
+	dbNameFlag         = flag.String("db-name", "", "Name of the database")
 	dbUsernameFlag     = flag.String("db-username", "", "Database username")
 	dbPasswordFlag     = flag.String("db-password", "", "Database password")
 	dbMaxOpenConnsFlag = flag.Int("db-max-open-conns", 25, "Maximum number of open database connections")
 	dbMaxIdleConnsFlag = flag.Int("db-max-idle-conns", 25, "Maximum number of idle database connections")
 
 	// S3
-	s3EndpointFlag        = flag.String("s3-endpoint", os.Getenv("S3_ENDPOINT"), "S3 endpoint")
-	s3BucketFlag          = flag.String("s3-bucket", os.Getenv("S3_BUCKET"), "S3 bucket")
-	s3RegionFlag          = flag.String("s3-region", os.Getenv("S3_REGION"), "S3 region")
+	s3EndpointFlag        = flag.String("s3-endpoint", "", "S3 endpoint")
+	s3BucketFlag          = flag.String("s3-bucket", "", "S3 bucket")
+	s3RegionFlag          = flag.String("s3-region", "", "S3 region")
 	s3AccessKeyFlag       = flag.String("s3-access-key", "", "S3 access key")
 	s3SecretKeyFlag       = flag.String("s3-secret-key", "", "S3 secret key")
 	s3TraceFlag           = flag.Bool("s3-trace", false, "Enable S3 HTTP tracing for debugging")
@@ -89,53 +95,242 @@ var (
 	metricsPasswordFlag = flag.String("metrics-password", "", "Metrics password")
 	metricsFlag         = flag.Bool("metrics", false, "Enable the metrics endpoint")
 	metricsAuthFlag     = flag.String("metrics-auth", "", "Set the auth type for the metrics endpoint")
-	metricsIdFlag       = flag.String("metrics-id", os.Getenv("METRICS_ID"), "Metrics instance identification")
+	metricsIdFlag       = flag.String("metrics-id", "", "Metrics instance identification")
 	metricsProxyURLFlag = flag.String("metrics-proxy-url", "", "URL to another Prometheus exporter that we should proxy")
 
 	// Slack integration
 	slackSecretFlag  = flag.String("slack-secret", "", "Slack secret (currently used to approve new bins via Slack if manual approval is enabled)")
-	slackDomainFlag  = flag.String("slack-domain", os.Getenv("SLACK_DOMAIN"), "Slack domain")
-	slackChannelFlag = flag.String("slack-channel", os.Getenv("SLACK_CHANNEL"), "Slack channel")
+	slackDomainFlag  = flag.String("slack-domain", "", "Slack domain")
+	slackChannelFlag = flag.String("slack-channel", "", "Slack channel")
+
+	// Version
+	versionFlag = flag.Bool("version", false, "Print version information and exit")
 )
 
 func main() {
 	flag.Parse()
 
-	// Set some default values that should not be exposed by flag.PrintDefaults()
+	if *versionFlag {
+		fmt.Printf("filebin %s (commit: %s)\n", version, commit)
+		os.Exit(0)
+	}
+
+	// Environment variable overrides (FILEBIN_ prefix)
+	// All configuration can be set via environment variables.
+	// Command line flags take precedence over environment variables.
+
+	// Various
+	if *contactFlag == "" {
+		*contactFlag = os.Getenv("FILEBIN_CONTACT")
+	}
+	if v := os.Getenv("FILEBIN_EXPIRATION"); v != "" && *expirationFlag == 604800 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*expirationFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_TMPDIR"); v != "" && *tmpdirFlag == os.TempDir() {
+		*tmpdirFlag = v
+	}
+	if v := os.Getenv("FILEBIN_TMPDIR_CAPACITY_THRESHOLD"); v != "" && *tmpdirThresholdFlag == 4.0 {
+		if f, err := strconv.ParseFloat(v, 64); err == nil {
+			*tmpdirThresholdFlag = f
+		}
+	}
+	if v := os.Getenv("FILEBIN_BASEURL"); v != "" && *baseURLFlag == "https://filebin.net" {
+		*baseURLFlag = v
+	}
+	if v := os.Getenv("FILEBIN_MANUAL_APPROVAL"); v != "" {
+		*requireApprovalFlag = v == "true" || v == "1" || v == "yes"
+	}
+	if v := os.Getenv("FILEBIN_REQUIRE_VERIFICATION_COOKIE"); v != "" {
+		*requireCookieFlag = v == "true" || v == "1" || v == "yes"
+	}
+	if v := os.Getenv("FILEBIN_VERIFICATION_COOKIE_LIFETIME"); v != "" && *cookieLifetimeFlag == 365 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*cookieLifetimeFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_EXPECTED_COOKIE_VALUE"); v != "" && *expectedCookieValueFlag == "2024-05-24" {
+		*expectedCookieValueFlag = v
+	}
+	if *mmdbCityPathFlag == "" {
+		*mmdbCityPathFlag = os.Getenv("FILEBIN_MMDB_CITY")
+	}
+	if *mmdbASNPathFlag == "" {
+		*mmdbASNPathFlag = os.Getenv("FILEBIN_MMDB_ASN")
+	}
+	if v := os.Getenv("FILEBIN_ALLOW_ROBOTS"); v != "" {
+		*allowRobotsFlag = v == "true" || v == "1" || v == "yes"
+	}
+
+	// Limits
+	if v := os.Getenv("FILEBIN_LIMIT_FILE_DOWNLOADS"); v != "" && *limitFileDownloadsFlag == 0 {
+		if i, err := strconv.ParseUint(v, 10, 64); err == nil {
+			*limitFileDownloadsFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_LIMIT_STORAGE"); v != "" && *limitStorageFlag == "0" {
+		*limitStorageFlag = v
+	}
+	if *rejectFileExtensions == "" {
+		*rejectFileExtensions = os.Getenv("FILEBIN_REJECT_FILE_EXTENSIONS")
+	}
+
+	// HTTP
+	if v := os.Getenv("FILEBIN_LISTEN_HOST"); v != "" && *listenHostFlag == "127.0.0.1" {
+		*listenHostFlag = v
+	}
+	if v := os.Getenv("FILEBIN_LISTEN_PORT"); v != "" && *listenPortFlag == 8080 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*listenPortFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_ACCESS_LOG"); v != "" && *accessLogFlag == "/var/log/filebin/access.log" {
+		*accessLogFlag = v
+	}
+	if v := os.Getenv("FILEBIN_PROXY_HEADERS"); v != "" {
+		*proxyHeadersFlag = v == "true" || v == "1" || v == "yes"
+	}
+
+	// Timeouts
+	if v := os.Getenv("FILEBIN_READ_TIMEOUT"); v != "" {
+		if d, err := time.ParseDuration(v); err == nil {
+			*readTimeoutFlag = d
+		}
+	}
+	if v := os.Getenv("FILEBIN_READ_HEADER_TIMEOUT"); v != "" {
+		if d, err := time.ParseDuration(v); err == nil {
+			*readHeaderTimeoutFlag = d
+		}
+	}
+	if v := os.Getenv("FILEBIN_WRITE_TIMEOUT"); v != "" {
+		if d, err := time.ParseDuration(v); err == nil {
+			*writeTimeoutFlag = d
+		}
+	}
+	if v := os.Getenv("FILEBIN_IDLE_TIMEOUT"); v != "" {
+		if d, err := time.ParseDuration(v); err == nil {
+			*idleTimeoutFlag = d
+		}
+	}
+
+	// Database
+	if *dbHostFlag == "" {
+		*dbHostFlag = os.Getenv("FILEBIN_DB_HOST")
+	}
+	if *dbPortFlag == "" {
+		*dbPortFlag = os.Getenv("FILEBIN_DB_PORT")
+	}
+	if *dbNameFlag == "" {
+		*dbNameFlag = os.Getenv("FILEBIN_DB_NAME")
+	}
 	if *dbUsernameFlag == "" {
-		*dbUsernameFlag = os.Getenv("DATABASE_USERNAME")
+		*dbUsernameFlag = os.Getenv("FILEBIN_DB_USERNAME")
 	}
 	if *dbPasswordFlag == "" {
-		*dbPasswordFlag = os.Getenv("DATABASE_PASSWORD")
+		*dbPasswordFlag = os.Getenv("FILEBIN_DB_PASSWORD")
+	}
+	if v := os.Getenv("FILEBIN_DB_MAX_OPEN_CONNS"); v != "" && *dbMaxOpenConnsFlag == 25 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*dbMaxOpenConnsFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_DB_MAX_IDLE_CONNS"); v != "" && *dbMaxIdleConnsFlag == 25 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*dbMaxIdleConnsFlag = i
+		}
+	}
+
+	// S3
+	if *s3EndpointFlag == "" {
+		*s3EndpointFlag = os.Getenv("FILEBIN_S3_ENDPOINT")
+	}
+	if *s3BucketFlag == "" {
+		*s3BucketFlag = os.Getenv("FILEBIN_S3_BUCKET")
+	}
+	if *s3RegionFlag == "" {
+		*s3RegionFlag = os.Getenv("FILEBIN_S3_REGION")
 	}
 	if *s3AccessKeyFlag == "" {
-		*s3AccessKeyFlag = os.Getenv("S3_ACCESS_KEY")
+		*s3AccessKeyFlag = os.Getenv("FILEBIN_S3_ACCESS_KEY")
 	}
 	if *s3SecretKeyFlag == "" {
-		*s3SecretKeyFlag = os.Getenv("S3_SECRET_KEY")
+		*s3SecretKeyFlag = os.Getenv("FILEBIN_S3_SECRET_KEY")
 	}
+	if v := os.Getenv("FILEBIN_S3_TRACE"); v != "" {
+		*s3TraceFlag = v == "true" || v == "1" || v == "yes"
+	}
+	if v := os.Getenv("FILEBIN_S3_SECURE"); v != "" {
+		*s3SecureFlag = v == "true" || v == "1" || v == "yes"
+	}
+	if v := os.Getenv("FILEBIN_S3_URL_TTL"); v != "" && *s3UrlTtlFlag == "1m" {
+		*s3UrlTtlFlag = v
+	}
+	if v := os.Getenv("FILEBIN_S3_TIMEOUT"); v != "" && *s3TimeoutFlag == "30s" {
+		*s3TimeoutFlag = v
+	}
+	if v := os.Getenv("FILEBIN_S3_TRANSFER_TIMEOUT"); v != "" && *s3TransferTimeoutFlag == "10m" {
+		*s3TransferTimeoutFlag = v
+	}
+
+	// Lurker
+	if v := os.Getenv("FILEBIN_LURKER_INTERVAL"); v != "" && *lurkerIntervalFlag == 300 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*lurkerIntervalFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_LURKER_THROTTLE"); v != "" && *lurkerThrottleFlag == 250 {
+		if i, err := strconv.Atoi(v); err == nil {
+			*lurkerThrottleFlag = i
+		}
+	}
+	if v := os.Getenv("FILEBIN_LOG_RETENTION"); v != "" && *logRetentionFlag == 7 {
+		if i, err := strconv.ParseUint(v, 10, 64); err == nil {
+			*logRetentionFlag = i
+		}
+	}
+
+	// Auth
 	if *adminUsernameFlag == "" {
-		*adminUsernameFlag = os.Getenv("ADMIN_USERNAME")
+		*adminUsernameFlag = os.Getenv("FILEBIN_ADMIN_USERNAME")
 	}
 	if *adminPasswordFlag == "" {
-		*adminPasswordFlag = os.Getenv("ADMIN_PASSWORD")
+		*adminPasswordFlag = os.Getenv("FILEBIN_ADMIN_PASSWORD")
 	}
 	if *metricsUsernameFlag == "" {
-		*metricsUsernameFlag = os.Getenv("METRICS_USERNAME")
+		*metricsUsernameFlag = os.Getenv("FILEBIN_METRICS_USERNAME")
 	}
 	if *metricsPasswordFlag == "" {
-		*metricsPasswordFlag = os.Getenv("METRICS_PASSWORD")
+		*metricsPasswordFlag = os.Getenv("FILEBIN_METRICS_PASSWORD")
+	}
+	if v := os.Getenv("FILEBIN_METRICS"); v != "" {
+		*metricsFlag = v == "true" || v == "1" || v == "yes"
 	}
 	if *metricsAuthFlag == "" {
-		*metricsAuthFlag = os.Getenv("METRICS_AUTH")
+		*metricsAuthFlag = os.Getenv("FILEBIN_METRICS_AUTH")
 	}
-	if *slackSecretFlag == "" {
-		*slackSecretFlag = os.Getenv("SLACK_SECRET")
+	if *metricsIdFlag == "" {
+		*metricsIdFlag = os.Getenv("FILEBIN_METRICS_ID")
 	}
 	if *metricsIdFlag == "" {
 		*metricsIdFlag = os.Getenv("HOSTNAME")
 	}
+	if *metricsProxyURLFlag == "" {
+		*metricsProxyURLFlag = os.Getenv("FILEBIN_METRICS_PROXY_URL")
+	}
 
+	// Slack integration
+	if *slackSecretFlag == "" {
+		*slackSecretFlag = os.Getenv("FILEBIN_SLACK_SECRET")
+	}
+	if *slackDomainFlag == "" {
+		*slackDomainFlag = os.Getenv("FILEBIN_SLACK_DOMAIN")
+	}
+	if *slackChannelFlag == "" {
+		*slackChannelFlag = os.Getenv("FILEBIN_SLACK_CHANNEL")
+	}
+
+	// Validate metrics proxy URL if set
 	if *metricsProxyURLFlag != "" {
 		_, err := url.Parse(*metricsProxyURLFlag)
 		if err != nil {
@@ -144,9 +339,9 @@ func main() {
 		}
 	}
 
-	// Contact information
+	// Contact information is required
 	if *contactFlag == "" {
-		fmt.Printf("Contact information, such as an email address, must be specified using the --contact parameter.\n")
+		fmt.Printf("Contact information must be specified using --contact or FILEBIN_CONTACT.\n")
 		os.Exit(2)
 	}
 
