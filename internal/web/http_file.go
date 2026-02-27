d81dfe1b6ac96a137cadcef761a09ee5c0635fcc
Espen Braastad <espen@varnish-software.com>
2026-02-27 22:56:29 +0100

    Remove X-Forwarded-For binding from presigned S3 URLs

--- internal/web/http_file.go
+++ internal/web/http_file.go
@@ -124,9 +124,7 @@ func (h *HTTP) getFile(w http.ResponseWriter, r *http.Request) {
 
 	// Redirect the client to a presigned URL for this fetch, which is more efficient
 	// than proxying the request through filebin.
-	// Bind the presigned URL to X-Forwarded-For if present.
-	clientIP := r.Header.Get("X-Forwarded-For")
-	presignedURL, err := h.s3.PresignedGetObject(file.SHA256, file.Filename, file.Mime, clientIP)
+	presignedURL, err := h.s3.PresignedGetObject(file.SHA256, file.Filename, file.Mime)
 	if err != nil {
 		h.Error(w, r, fmt.Sprintf("Unable to generate presigned URL for bin %q and filename %q: %s", inputBin, inputFilename, err.Error()), "Unable to presign URL for object", 1351, http.StatusInternalServerError)
 		return
